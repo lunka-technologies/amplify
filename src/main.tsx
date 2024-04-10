3a4cae5accf964d83468c03dbb57072c05d29a5e
anastasia7makar <anastasia7makar@gmail.com>
2024-04-10 21:45:39 +0300

    Add router

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router";
 
 ReactDOM.createRoot(document.getElementById("amplifi")!).render(
-  <React.StrictMode>Hello world</React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
