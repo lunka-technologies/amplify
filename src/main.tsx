4658fd84623ce6b6905637ba7bf15059575740d1
anastasia7makar <anastasia7makar@gmail.com>
2024-04-10 21:38:35 +0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("amplifi")!).render(
+  <React.StrictMode>Hello world</React.StrictMode>
+);
