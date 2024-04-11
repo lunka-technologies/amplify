74c5c6577efb321b01d6284641f76489fbbf8720
anastasia7makar <anastasia7makar@gmail.com>
2024-04-11 13:43:13 +0300

    Add button

---
+++ src/components/button/button.tsx
@@ -0,0 +1,33 @@
+import styles from './button.module.scss';
+import classNames from 'classnames';
+import { ReactNode } from 'react';
+
+interface IButtonProps {
+  onClick?: () => void;
+  color: 'mint';
+  children: ReactNode;
+  className?: string;
+  type?: 'button' | 'submit' | 'reset' | undefined;
+}
+
+export const Button = ({
+  onClick = () => null,
+  color = 'mint',
+  children,
+  className,
+  type = 'button',
+}: IButtonProps) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={classNames([
+        className,
+        styles.button,
+        styles[`color_${color}`],
+      ])}
+    >
+      {children}
+    </button>
+  );
+};
