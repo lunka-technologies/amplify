110eb73c5e6f0ed04ce9b852b5e3dc771d0a19af
anastasia7makar <anastasia7makar@gmail.com>
2024-05-14 12:55:11 +0300

    add request for get user

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,19 +1,35 @@
+import { apis } from '../../axios/apis';
+import { axiosInstance } from '../../axios/instance';
 import { Card } from '../card/card';
 import { MenuItems } from '../menuItems/menuItems';
 import styles from './profile.module.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+interface IProfileType {
+  email: string;
+  username: string;
+}
 
 export const Profile = () => {
-  const [userData] = useState({
-    username: 'Jeff_0x',
-    email: 'jeff.cafolla@gmail.com',
-    avatarSrc: '/pictures/user-avatar.png',
+  const [userData, setUserData] = useState<IProfileType>({
+    email: '',
+    username: '',
   });
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  const fetchUser = async () => {
+    const {
+      data: { payload },
+    } = await axiosInstance.get(apis.getUser, {});
+    setUserData(payload);
+  };
 
   return (
     <Card className={styles.profileContainer}>
       <div className={styles.dataContainer}>
-        <img src={userData.avatarSrc} alt="avatar" className={styles.avatar} />
         <div className={styles.container}>
           <p className={styles.username}>{userData.username}</p>
           <p className={styles.email}>{userData.email}</p>
