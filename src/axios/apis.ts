9cb5d03444b4940a03bf2283fdf148fbda5214f7
anastasia7makar <anastasia7makar@gmail.com>
2024-05-14 12:50:23 +0300

    set up axios

---
+++ src/axios/apis.ts
@@ -0,0 +1,13 @@
+export const apis = {
+  signup: '/auth/signup',
+  login: '/auth/login',
+  getUser: '/auth/user',
+  stake: '/strategy/testnet/stake',
+  halt: '/strategy/testnet/halt',
+  info: '/strategy/testnet/info',
+  createWallet: '/wallet/create',
+  getWallet: '/wallet/get',
+  postFund: '/wallet/fund',
+  getBalance: '/wallet/balance',
+  postWithdraw: '/wallet/withdraw',
+};
---
+++ src/axios/instance.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 10000,
+  headers: { Authorization: `Bearer ${localStorage}` },
+  //   withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use((request) => {
+  const jwtToken = localStorage.getItem('jwt-token');
+
+  if (jwtToken) request.headers.set('Authorization', `Bearer ${jwtToken}`);
+
+  return request;
+});
+
+// axiosInstance.interceptors.response.use(
+//   function (response) {
+//     return response;
+//   },
+//   function (error) {
+//     if (error instanceof AxiosError) {
+//       // Unauthorized
+//       if (error.response?.status === 401) {
+//         router.navigate(routes.login);
+//       }
+//     }
+//     console.log(error);
+
+//     return Promise.reject(error);
+//   }
+// );
