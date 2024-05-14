4796229fe87e4c2ea560b1e4f00f7671a22d4638
anastasia7makar <anastasia7makar@gmail.com>
2024-05-14 12:57:50 +0300

    add logout function

--- src/components/menuItems/menuItems.tsx
+++ src/components/menuItems/menuItems.tsx
@@ -2,11 +2,18 @@ import HelpSVG from '../../assets/help-circle.svg?react';
 import LogoutSVG from '../../assets/logout.svg?react';
 import SettingSVG from '../../assets/setting.svg?react';
 import { ChangePasswordForm } from '../../forms/changePassword/changePasswordForm';
+import { ROUTE_MAIN } from '../../router/routes';
 import styles from './menuItems.module.scss';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const MenuItems = () => {
   const [isShowChangePassword, setShowChangePassword] = useState(false);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem('jwt-token');
+    navigate(ROUTE_MAIN);
+  };
 
   return (
     <ul className={styles.list}>
@@ -23,7 +30,12 @@ export const MenuItems = () => {
         <HelpSVG />
         <span className={styles.label}>Help</span>
       </li>
-      <li className={styles.item} onClick={() => {}}>
+      <li
+        className={styles.item}
+        onClick={() => {
+          handleLogout();
+        }}
+      >
         <LogoutSVG />
         <span className={styles.label}>Logout</span>
       </li>
