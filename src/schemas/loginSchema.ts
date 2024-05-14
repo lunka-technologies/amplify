2f4cc4d2eb104846e8b59ff1f9e89374f485a51e
anastasia7makar <anastasia7makar@gmail.com>
2024-05-14 12:48:56 +0300

    refactor schemas

--- src/schemas/loginSchema.ts
+++ src/schemas/loginSchema.ts
@@ -5,7 +5,7 @@ export const loginSchema = Yup.object().shape({
   password: Yup.string()
     .required('Password is required')
     .min(8, 'Password must be at least 8 characters long')
-    .max(16, 'Password cannot exceed 16 characters'),
+    .max(30, 'Password cannot exceed 30 characters'),
 });
 
 export type LoginSchema = Yup.InferType<typeof loginSchema>;
--- src/schemas/registerSchema.ts
+++ src/schemas/registerSchema.ts
@@ -1,15 +1,13 @@
 import * as Yup from 'yup';
 
 export const registerSchema = Yup.object().shape({
-  name: Yup.string().required('Name is required'),
+  name: Yup.string(),
   email: Yup.string().email('Invalid email').required('Email is required'),
   discordUsername: Yup.string().required('Discord username is required'),
   password: Yup.string()
-    .min(6)
-    .max(32)
-    .required('Password is required')
     .min(8, 'Password must be at least 8 characters long')
-    .max(16, 'Password cannot exceed 16 characters'),
+    .max(30, 'Password cannot exceed 30 characters')
+    .required('Password is required'),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref('password')], 'Passwords must match')
     .required('Please confirm your password'),
