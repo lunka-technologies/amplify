efb72079fb67d16586760b257c29f4e65fff69b0
anastasia7makar <anastasia7makar@gmail.com>
2024-05-14 18:07:52 +0300

    add success modal to wallet

--- src/components/wallet/wallet.tsx
+++ src/components/wallet/wallet.tsx
@@ -25,6 +25,7 @@ export const Wallet = ({
   setWalletOpen,
 }: IWalletProps) => {
   const [withdrawData, setWithdrawData] = useState('');
+  const [isSuccessful, setIsSuccessful] = useState(false);
 
   const fetchWallet = async (address: string) => {
     try {
@@ -32,6 +33,8 @@ export const Wallet = ({
         address,
         amount,
       });
+      await axiosInstance.get(apis.getBalance);
+      setIsSuccessful(true);
     } catch (error) {
       if (error instanceof AxiosError) {
         console.log(error);
@@ -60,59 +63,105 @@ export const Wallet = ({
 
   const handleWalletClose = () => {
     setWalletOpen(!isWalletOpen);
+    setIsSuccessful(false);
   };
 
   const handleWithdraw = (withdrawData: string) => {
-    console.log('Withdraw Data:', withdrawData);
     fetchWallet(withdrawData);
-    setWalletOpen(false);
+  };
+
+  const handleGoBack = () => {
+    setIsSuccessful(false);
   };
 
   return (
     <Card className={styles.walletContainer}>
-      <div className={styles.wallet}>
-        <CloseSVG className={styles.closeButton} onClick={handleWalletClose} />
-        <p className={styles.walletText}>
-          Deposit: You can deposit into this account
-        </p>
-        <div className={styles.deposit}>
-          <div className={styles.keyContainer}>
-            <KeySVG />
-            <div className={styles.textContainer}>
-              <h2 className={styles.title}>Deposit Wallet</h2>
-              <p className={styles.key}>{wallet}</p>
+      {isSuccessful ? (
+        <>
+          <div className={styles.wallet}>
+            <CloseSVG
+              className={styles.closeButton}
+              onClick={handleWalletClose}
+            />
+
+            <div className={styles.success}>
+              <h2 className={styles.successTitle}>Successfully Sent</h2>
+              <p className={styles.successMessage}>
+                Deposit: Was successfully sent to address
+              </p>
+              <p className={styles.successKey}>{withdrawData}</p>
             </div>
           </div>
-          <CopySVG className={styles.copyButton} onClick={handleCopyClick} />
-        </div>
-        <div className={styles.divider} />
-        <p className={styles.walletText}>Insert Wallet to withdraw to</p>
-        <Input
-          id="wallet"
-          placeholder="Add your Withdraw Wallet"
-          value={withdrawData}
-          onChange={(e: SyntheticEvent<HTMLInputElement>) =>
-            setWithdrawData(e.currentTarget.value)
-          }
-        />
-      </div>
-      <div className={styles.buttonsGroup}>
-        <Button
-          color="dark"
-          className={styles.button}
-          onClick={handleWalletClose}
-        >
-          Close
-        </Button>
-        <Button
-          color="mint"
-          className={styles.button}
-          onClick={() => handleWithdraw(withdrawData)}
-        >
-          <WithdrawSVG />
-          Withdraw
-        </Button>
-      </div>
+          <div className={styles.buttonsGroup}>
+            <Button
+              color="dark"
+              className={styles.button}
+              onClick={handleWalletClose}
+            >
+              Close
+            </Button>
+            <Button
+              color="mint"
+              className={styles.button}
+              onClick={() => handleGoBack()}
+            >
+              Go Back
+            </Button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className={styles.wallet}>
+            <CloseSVG
+              className={styles.closeButton}
+              onClick={handleWalletClose}
+            />
+            <p className={styles.walletText}>
+              Deposit: You can deposit into this account
+            </p>
+            <div className={styles.deposit}>
+              <div className={styles.keyContainer}>
+                <KeySVG />
+                <div className={styles.textContainer}>
+                  <h2 className={styles.title}>Deposit Wallet</h2>
+                  <p className={styles.key}>{wallet}</p>
+                </div>
+              </div>
+              <CopySVG
+                className={styles.copyButton}
+                onClick={handleCopyClick}
+              />
+            </div>
+            <div className={styles.divider} />
+            <p className={styles.walletText}>Insert Wallet to withdraw to</p>
+            <Input
+              id="wallet"
+              placeholder="Add your Withdraw Wallet"
+              value={withdrawData}
+              onChange={(e: SyntheticEvent<HTMLInputElement>) =>
+                setWithdrawData(e.currentTarget.value)
+              }
+            />
+          </div>
+          <div className={styles.buttonsGroup}>
+            <Button
+              color="dark"
+              className={styles.button}
+              onClick={handleWalletClose}
+            >
+              Close
+            </Button>
+            <Button
+              color="mint"
+              className={styles.button}
+              onClick={() => handleWithdraw(withdrawData)}
+            >
+              <WithdrawSVG />
+              Withdraw
+            </Button>
+          </div>
+        </>
+      )}
     </Card>
   );
 };
