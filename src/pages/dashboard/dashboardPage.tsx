5f3c948ff4328149151cd18de089fb0a0c836c90
anastasia7makar <anastasia7makar@gmail.com>
2024-05-14 12:53:10 +0300

    add requests for wallet

--- src/pages/dashboard/dashboardPage.tsx
+++ src/pages/dashboard/dashboardPage.tsx
@@ -1,14 +1,52 @@
+import { apis } from '../../axios/apis';
+import { axiosInstance } from '../../axios/instance';
 import { LineChart } from '../../components/charts/lineChart/lineChart';
 import { Dropdown } from '../../components/dropdown/dropdown';
 import { Header } from '../../components/header/header';
 import { Switch } from '../../components/switch/switch';
 import { Table } from '../../components/table/table';
 import styles from './dashboardPage.module.scss';
+import { AxiosError } from 'axios';
+import { useEffect, useState } from 'react';
 
 export const DashboardPage = () => {
+  const [walletData, setWalletData] = useState<string>('');
+  const [amount, setAmount] = useState(0);
+
+  const fetchWallet = async () => {
+    try {
+      const {
+        data: { address },
+      } = await axiosInstance.get(apis.getWallet);
+
+      if (!address) {
+        await axiosInstance.post(apis.createWallet, {});
+        await axiosInstance.post(apis.postFund, {});
+      } else {
+        setWalletData(address);
+      }
+
+      const {
+        data: { USDTBalance },
+      } = await axiosInstance.get(apis.getBalance, {});
+
+      setAmount(USDTBalance);
+
+      await axiosInstance.get(apis.info, {});
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        console.log(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchWallet();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <Header />
+      <Header wallet={walletData} amount={amount} />
 
       <div className={styles.boxContainer}>
         <div className={styles.chart}>
@@ -28,7 +66,7 @@ export const DashboardPage = () => {
               <Dropdown />
             </div>
           </div>
-          <Table />
+          <Table amount={amount} />
         </div>
       </div>
     </div>
