bcbe2ee8fab7d17f740ad274018019e121ab62e1
anastasia7makar <anastasia7makar@gmail.com>
2024-05-14 18:09:39 +0300

    add success modal to withdraw

--- src/modals/withdrawModal/withdrawModal.tsx
+++ src/modals/withdrawModal/withdrawModal.tsx
@@ -1,4 +1,5 @@
 import CloseSVG from '../../assets/close-icon.svg?react';
+import WithdrawSVG from '../../assets/icon-withdraw.svg?react';
 import { apis } from '../../axios/apis';
 import { axiosInstance } from '../../axios/instance';
 import { Button } from '../../components/button/button';
@@ -25,12 +26,14 @@ export const WithdrawModal = ({
   const persentage = 0;
 
   const [inputValue, setInputValue] = useState('0.0');
+  const [isSuccessful, setSuccessful] = useState(false);
 
   const fetchWithdraw = async () => {
     try {
       await axiosInstance.post(apis.halt, {
         amount: Number(inputValue),
       });
+      setSuccessful(true);
     } catch (error) {
       if (error instanceof AxiosError) {
         console.log(error);
@@ -49,51 +52,84 @@ export const WithdrawModal = ({
     }
   };
 
+  const handleGoBack = () => {
+    setSuccessful(false);
+  };
+
   return (
     <Modal>
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <CloseSVG className={styles.closeButton} onClick={handleCloseModal} />
-          <h1 className={styles.title}>Withdraw</h1>
-          <p className={styles.subtitle}>Choose amount and token to withdraw</p>
+      {isSuccessful ? (
+        <div className={styles.container}>
+          <div className={styles.header}>
+            <CloseSVG
+              className={styles.closeButton}
+              onClick={handleCloseModal}
+            />
+          </div>
+          <div className={styles.success}>
+            <WithdrawSVG />
+            <h1 className={styles.successTitle}>Successfully Withdraw</h1>
+            <p className={styles.successMessage}>Withdraw: Was successfully</p>
+          </div>
+          <div className={styles.actions}>
+            <Button color="dark" onClick={handleGoBack}>
+              Go Back
+            </Button>
+            <Button color="mint" onClick={handleCloseModal}>
+              Close
+            </Button>
+          </div>
         </div>
-        <div className={styles.divider} />
-        <div className={styles.body}>
-          <div className={styles.amountContainer}>
-            {' '}
-            <h2 className={styles.amountLabel}>Amount</h2>
-            <p className={styles.amountText}>
-              <span>{maxAmount}</span>
-              {'  '} USDT available
+      ) : (
+        <div className={styles.container}>
+          <div className={styles.header}>
+            <CloseSVG
+              className={styles.closeButton}
+              onClick={handleCloseModal}
+            />
+            <h1 className={styles.title}>Withdraw</h1>
+            <p className={styles.subtitle}>
+              Choose amount and token to withdraw
             </p>
           </div>
-          <div className={styles.inputContainer}>
-            <Input
-              onRef={inputRef}
-              onChange={(e) => setInputValue(e.currentTarget.value)}
-              maxValue={maxAmount}
-              className={styles.input}
-              id="amount"
-              type="amount"
-              placeholder="Enter amount"
-            />
-            <div className={styles.maxButton} onClick={handleMaxAmount}>
-              MAX
+          <div className={styles.divider} />
+          <div className={styles.body}>
+            <div className={styles.amountContainer}>
+              {' '}
+              <h2 className={styles.amountLabel}>Amount</h2>
+              <p className={styles.amountText}>
+                <span>{maxAmount}</span>
+                {'  '} USDT available
+              </p>
             </div>
+            <div className={styles.inputContainer}>
+              <Input
+                onRef={inputRef}
+                onChange={(e) => setInputValue(e.currentTarget.value)}
+                maxValue={maxAmount}
+                className={styles.input}
+                id="amount"
+                type="amount"
+                placeholder="Enter amount"
+              />
+              <div className={styles.maxButton} onClick={handleMaxAmount}>
+                MAX
+              </div>
+            </div>
+            <p className={styles.subtitle}>
+              Average APY invested : {'  '} {persentage}%
+            </p>
           </div>
-          <p className={styles.subtitle}>
-            Average APY invested : {'  '} {persentage}%
-          </p>
+          <div className={styles.actions}>
+            <Button color="dark" onClick={handleCloseModal}>
+              Go Back
+            </Button>
+            <Button color="mint" onClick={fetchWithdraw}>
+              Withdraw
+            </Button>
+          </div>
         </div>
-        <div className={styles.actions}>
-          <Button color="dark" onClick={handleCloseModal}>
-            Go Back
-          </Button>
-          <Button color="mint" onClick={fetchWithdraw}>
-            Withdraw
-          </Button>
-        </div>
-      </div>
+      )}
     </Modal>
   );
 };
