d7e0ae1cd55495c060d4b2811c719655c4d94e80
anastasia7makar <anastasia7makar@gmail.com>
2024-06-10 14:21:13 +0300

    fix base

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -7,16 +7,19 @@ import { RootLayout } from './rootLayout';
 import { ROUTE_DASHBOARD, ROUTE_MAIN, ROUTE_REGISTER } from './routes';
 import { createBrowserRouter } from 'react-router-dom';
 
-export const router = createBrowserRouter([
-  {
-    element: <RootLayout />,
-    children: [
-      { path: ROUTE_MAIN, element: <LoginPage /> },
-      { path: ROUTE_REGISTER, element: <RegisterPage /> },
-    ],
-  },
-  {
-    element: <PrivateLayout />,
-    children: [{ path: ROUTE_DASHBOARD, element: <DashboardPage /> }],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      element: <RootLayout />,
+      children: [
+        { path: ROUTE_MAIN, element: <LoginPage /> },
+        { path: ROUTE_REGISTER, element: <RegisterPage /> },
+      ],
+    },
+    {
+      element: <PrivateLayout />,
+      children: [{ path: ROUTE_DASHBOARD, element: <DashboardPage /> }],
+    },
+  ],
+  { basename: '/amplify/' }
+);
